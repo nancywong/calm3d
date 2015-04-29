f7b9b3e944b3da15318b69fbe277e7eb272cdeb8
nancywong <nancypwong@gmail.com>
2015-04-29 03:04:52 -0400

    nice working demo breakpoint - m to mute, ocean shader, sounds, looks nice

--- app/assets/js/listeners.js
+++ app/assets/js/listeners.js
@@ -8,21 +8,12 @@ var toggleMute = function () {
 
   if (isMute) {
     // unmute
-//    $('#sounds').get(0).play();
     sounds.play();
-    console.log('not muted: ' + sounds.src);
-    console.log($instructions.html());
     $instructions.html('m to mute');
-    console.log('$instructions read: ' + $instructions.html());
-    console.log('?');
   } else {
     // mute sounds
-    //$('#sounds').get(0).pause();
     sounds.pause();
-    console.log('mmuted src:' + sounds.src);
     $instructions.html('m to unmute');
-    console.log('$instructions read: ' + $instructions.html());
-    console.log('?');
   }
   isMute = !isMute;
   
